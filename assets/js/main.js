36f1afe93a3cd12fa8fd0f774d5e0ed24e975727
Sparky <contact@protosparky.uk>
2024-07-31 22:24:27 +0200

    changed internal serv config

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
 function init(){
     console.info("init run");
+    const docroot = document.getElementById("content-fullscreen");
+    const image = document.createElement("img");
+    image.style.position = "absolute";
+    image.style.left = "50%";
+    image.style.width  ="auto";
+    image.style.height = "100%";
+    image.style.top = "0px";
+    image.style.transform = "translate(-50%, 0)";
+    image.src = "./assets/img/static.png";
+    docroot.appendChild(image);
 }
--- cam.py
+++ cam.py
@@ -6,12 +6,19 @@ import cv2
 import pyassets.tools as tools 
 import http.server  
 import socketserver
+import threading
+import time
+
+
 try:
     settings = tools.open_json(settings_dir)
 except:
     print("Settings file missing! Run setup.py")
     exit()
-tools.launch_html_server("./",4200)
+
+html_serv_thread = threading.Thread(target=tools.launch_html_server("./",4220))
+html_serv_thread.start()
+
 # Initialize the camera
 cam = cv2.VideoCapture(int(settings["cam_id"]))
 
